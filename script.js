22595a5b36c1858833ddb8d93795344a1e9b9205
Abhishek Kumar Gupta <badabhishekgupta@gmail.com>
2023-05-03 18:38:51 +0530

    updating code

--- script.js
+++ script.js
@@ -22,20 +22,21 @@ async function loadTasks() {
       <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1" id="title-${element._id}" >${element.title}</h5>
       
+      <div>
       <button type="button" class="btn btn-sm btn-secondary" onclick="updateTask('${element._id}')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-    </svg>Edit
+    </svg>
         </button>
 
       <button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${element._id}')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>Delete
+</svg>
         </button>
-
+      </div>
       </div>
       <p class="mb-1" id="description-${element._id}" >${element.description}</p>
       </a>
@@ -82,19 +83,23 @@ async function addTask() {
   var listTemplate = ` <a href="#" id="a-${newList._id}"class="conatiner list-group-item list-group-item-action active " aria-current="true">
  <div class="d-flex w-100 justify-content-between">
  <h5 class="mb-1" id="title-${newList._id}" >${newList.title}</h5>
+
+ <div>
  <button type="button" class="btn btn-sm btn-secondary" id="editTaskBtn-${newList._id}" onclick="updateTask('${newList._id}')">
  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-</svg>Edit
+</svg>
 </button>
 
 <button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${newList._id}')">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>Delete
+</svg>
 </button>
+ </div>
+ 
 
  </div>
  <p class="mb-1" id="description-${newList._id}" >${newList.description}</p>
@@ -129,9 +134,9 @@ async function getTaskByID(taskID) {
 }
 
 async function updateTask(task_id) {
-  document.getElementById("addTaskBtn").setAttribute("disabled", "true");
+  document.getElementById("addTaskBtn").classList.add("d-none");
   const updateBtnElmRef = document.getElementById("updateTaskBtn");
-  updateBtnElmRef.removeAttribute("disabled");
+  updateBtnElmRef.classList.remove("d-none");
 
   const task = await getTaskByID(task_id);
 
@@ -152,8 +157,8 @@ async function updateTask(task_id) {
       document.getElementById("title").value = "";
       document.getElementById("desc").value = "";
 
-      document.getElementById("updateTaskBtn").setAttribute("disabled", "true");
-      document.getElementById("addTaskBtn").removeAttribute("disabled");
+      document.getElementById("updateTaskBtn").classList.add("d-none");
+      document.getElementById("addTaskBtn").classList.remove("d-none");
 
       document.getElementById("title-" + task_id).innerHTML = title;
       document.getElementById("description-" + task_id).innerHTML = description;
