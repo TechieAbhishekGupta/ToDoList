b8c29264e0356472d6d5f46bbf1e4e7f32e3c200
shivamsahray <gamingguruji1994@gmail.com>
2023-05-03 15:14:36 +0530

    added ui

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", loadTasks);
 
 const CRUD_BASEURL =
-  "https://crudcrud.com/api/5f15417feb4642aa9b57396fb3aa9474/";
+  "https://crudcrud.com/api/be0098b3cd1b4cb087cb228bae88d236/";
 
 var taskListjson;
 var taskListRef;
@@ -16,10 +16,10 @@ async function loadTasks() {
       console.log(element);
 
       var listTemplate = `
-      <a href="#" id="a-${element._id}" data-taskID="${element._id}" class="list-group-item list-group-item-action" aria-current="true">
+      <a href="#" id="a-${element._id}" data-taskID="${element._id}" class="container list-group-item list-group-item-action" aria-current="true">
       <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1" id="title-${element._id}" >${element.title}</h5>
-      <button type="button" class="btn btn-sm btn-secondary" onclick="updateTask('${element._id}')">
+      <button type="button" class="btn btn-sm btn-secondary " onclick="updateTask('${element._id}')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -76,7 +76,7 @@ async function addTask() {
     description: description,
   });
 
-  var listTemplate = ` <a href="#" id="a-${newList._id}"class="list-group-item list-group-item-action active" aria-current="true">
+  var listTemplate = ` <a href="#" id="a-${newList._id}"class="conatiner list-group-item list-group-item-action active " aria-current="true">
  <div class="d-flex w-100 justify-content-between">
  <h5 class="mb-1" id="title-${newList._id}" >${newList.title}</h5>
  <button type="button" class="btn btn-sm btn-secondary" id="editTaskBtn-${newList._id}" onclick="updateTask('${newList._id}')">
