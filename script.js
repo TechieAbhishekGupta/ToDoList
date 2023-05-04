3a28443aea22105025375a4305f2e1e536ca9175
Ashish lamba <Ashishlamba512@gmail.com>
2023-05-04 16:59:19 +0530

    Added Date Feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", loadTasks);
 
 const CRUD_BASEURL =
-  "https://crudcrud.com/api/5f15417feb4642aa9b57396fb3aa9474/";
+  "https://crudcrud.com/api/64a6b6099bd44ead8a637e2dbd418a44/";
 
 var taskListjson;
 var taskListRef;
@@ -9,30 +9,35 @@ var taskListRef;
 async function loadTasks() {
   taskListRef = document.getElementById("taskList");
 
-  taskListRef.innerHTML = fetch(CRUD_BASEURL + "tasks").then(async (el) => {
+  taskListRef.innerHTML = "";
+
+  fetch(CRUD_BASEURL + "tasks").then(async (el) => {
     taskListjson = await el.json();
 
     taskListjson.forEach((element) => {
       console.log(element);
 
       var listTemplate = `
-      <a href="#" id="a-${element._id}" data-taskID="${element._id}" class="list-group-item list-group-item-action" aria-current="true">
+      <a href="#" id="a-${element._id}" data-taskID="${element._id}" class="container list-group-item list-group-item-action" aria-current="true">
       <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1" id="title-${element._id}" >${element.title}</h5>
+      <small id="date-${newList._id}">${newList.date}</small>
+      
+      <div>
       <button type="button" class="btn btn-sm btn-secondary" onclick="updateTask('${element._id}')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-    </svg>Edit
+    </svg>
         </button>
 
       <button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${element._id}')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>Delete
+</svg>
         </button>
-
+      </div>
       </div>
       <p class="mb-1" id="description-${element._id}" >${element.description}</p>
       </a>
@@ -43,7 +48,7 @@ async function loadTasks() {
   });
 }
 
-function isTaskListValid(title, description) {
+function isTaskListValid(title, description, date) {
   let isFormValid = true;
 
   if (!title) {
@@ -53,6 +58,11 @@ function isTaskListValid(title, description) {
 
   if (!description) {
     document.getElementById("desc-error").classList.remove("visually-hidden");
+    isFormValid = false;
+  }
+
+  if (!date) {
+    document.getElementById("date-error").classList.remove("visually-hidden");
     isFormValid = false;
   }
 
@@ -68,30 +78,36 @@ async function addTask() {
   // write add task logic here
   const title = document.getElementById("title").value;
   const description = document.getElementById("desc").value;
+  const date = document.getElementById("date").value;
 
-  isTaskListValid(title, description);
+  isTaskListValid(title, description, date);
 
   const newList = await postList({
     title: title,
     description: description,
+    date: date,
   });
 
-  var listTemplate = ` <a href="#" id="a-${newList._id}"class="list-group-item list-group-item-action active" aria-current="true">
+  var listTemplate = ` <a href="#" id="a-${newList._id}"class="conatiner list-group-item list-group-item-action active " aria-current="true">
  <div class="d-flex w-100 justify-content-between">
  <h5 class="mb-1" id="title-${newList._id}" >${newList.title}</h5>
+ <small id="date-${newList._id}">${newList.date}</small>
+ <div>
  <button type="button" class="btn btn-sm btn-secondary" id="editTaskBtn-${newList._id}" onclick="updateTask('${newList._id}')">
  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-</svg>Edit
+</svg>
 </button>
 
 <button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${newList._id}')">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>Delete
+</svg>
 </button>
+ </div>
+ 
 
  </div>
  <p class="mb-1" id="description-${newList._id}" >${newList.description}</p>
@@ -102,6 +118,7 @@ async function addTask() {
 
   document.getElementById("title").value = "";
   document.getElementById("desc").value = "";
+  document.getElementById("date").value = "";
 }
 
 async function postList(list) {
@@ -126,34 +143,39 @@ async function getTaskByID(taskID) {
 }
 
 async function updateTask(task_id) {
-  document.getElementById("addTaskBtn").setAttribute("disabled", "true");
+  document.getElementById("addTaskBtn").classList.add("d-none");
   const updateBtnElmRef = document.getElementById("updateTaskBtn");
-  updateBtnElmRef.removeAttribute("disabled");
+  updateBtnElmRef.classList.remove("d-none");
 
   const task = await getTaskByID(task_id);
 
   document.getElementById("title").value = task.title;
   document.getElementById("desc").value = task.description;
+  document.getElementById("date").value = task.date;
 
   updateBtnElmRef.addEventListener("click", async function () {
     const title = document.getElementById("title").value;
     const description = document.getElementById("desc").value;
+    const date = document.getElementById("date").value;
 
-    isTaskListValid(title, description);
+    isTaskListValid(title, description, date);
 
     const isUpdated = await updateTaskData(task_id, {
       title,
       description,
+      date,
     });
     if (isUpdated) {
       document.getElementById("title").value = "";
       document.getElementById("desc").value = "";
+      document.getElementById("date").value = "";
 
-      document.getElementById("updateTaskBtn").setAttribute("disabled", "true");
-      document.getElementById("addTaskBtn").removeAttribute("disabled");
+      document.getElementById("updateTaskBtn").classList.add("d-none");
+      document.getElementById("addTaskBtn").classList.remove("d-none");
 
       document.getElementById("title-" + task_id).innerHTML = title;
       document.getElementById("description-" + task_id).innerHTML = description;
+      document.getElementById("date-" + task_id).innerHTML = date;
     } else {
       alert("Task not updated");
     }
