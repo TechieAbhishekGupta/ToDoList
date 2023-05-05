a061b36dc68fb05ae09131f5ddc2f9e2090a03a8
Abhishek Kumar Gupta <badabhishekgupta@gmail.com>
2023-05-06 03:38:25 +0530

    Search feature added

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 window.addEventListener("load", loadTasks);
 
 const CRUD_BASEURL =
-  "https://crudcrud.com/api/64a6b6099bd44ead8a637e2dbd418a44/";
+  "https://crudcrud.com/api/fe8874ec9f0040f6aa0a4e155fd48f04/";
 
 var taskListjson;
 var taskListRef;
+
+function renderListItem(element, isNewTask) {
+  var listTemplate = `
+      <a href="#" id="a-${element._id}" data-taskID="${
+    element._id
+  }" class="container list-group-item list-group-item-action ${
+    isNewTask ? "active" : ""
+  }" aria-current="true">
+      
+      <div class="d-flex w-100 justify-content-between">
+      
+      <h5 class="mb-1" id="title-${element._id}" >${element.title}</h5>
+      <small id="date-${element._id}">${element.date}</small>
+      
+      <div>
+      <button type="button" class="btn btn-sm btn-secondary" onclick="updateTask('${
+        element._id
+      }')">
+      <i class="bi bi-pencil-square"></i>
+      </button>
+      
+      <button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${
+        element._id
+      }')">
+      <i class="bi bi-x-square"></i>
+      </button>
+      </div>
+
+      </div>
+      
+      <p class="mb-1" id="description-${element._id}">${element.description}</p>
+      </a>
+      `;
+
+  return listTemplate;
+}
 
 async function loadTasks() {
   taskListRef = document.getElementById("taskList");
@@ -17,32 +53,7 @@ async function loadTasks() {
     taskListjson.forEach((element) => {
       console.log(element);
 
-      var listTemplate = `
-      <a href="#" id="a-${element._id}" data-taskID="${element._id}" class="container list-group-item list-group-item-action" aria-current="true">
-      <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1" id="title-${element._id}" >${element.title}</h5>
-      <small id="date-${element._id}">${element.date}</small>
-      
-      <div>
-      <button type="button" class="btn btn-sm btn-secondary" onclick="updateTask('${element._id}')">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-    </svg>
-        </button>
-
-      <button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${element._id}')">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>
-        </button>
-      </div>
-      </div>
-      <p class="mb-1" id="description-${element._id}" >${element.description}</p>
-      </a>
-      `;
-
+      var listTemplate = renderListItem(element, false);
       taskListRef.innerHTML += listTemplate;
     });
   });
@@ -70,12 +81,12 @@ function isTaskListValid(title, description, date) {
     return;
   }
 }
+
 const addTaskBtnRef = document.getElementById("addTaskBtn");
 
 addTaskBtnRef.addEventListener("click", addTask);
 
 async function addTask() {
-  // write add task logic here
   const title = document.getElementById("title").value;
   const description = document.getElementById("desc").value;
   const date = document.getElementById("date").value;
@@ -88,33 +99,9 @@ async function addTask() {
     date: date,
   });
 
-  var listTemplate = ` <a href="#" id="a-${newList._id}" class="container list-group-item list-group-item-action active " aria-current="true">
- <div class="d-flex w-100 justify-content-between">
- <h5 class="mb-1" id="title-${newList._id}" >${newList.title}</h5>
- <small id="date-${newList._id}">${newList.date}</small>
-
- <div>
- <button type="button" class="btn btn-sm btn-secondary" id="editTaskBtn-${newList._id}" onclick="updateTask('${newList._id}')">
- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-</svg>
-</button>
-
-<button type="button" class="btn btn-sm btn-danger" onclick="deleteTaskData('${newList._id}')">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-</svg>
-</button>
- </div>
- 
-
- </div>
- <p class="mb-1" id="description-${newList._id}" >${newList.description}</p>
- </a>`;
   taskListjson.push(newList);
 
+  var listTemplate = renderListItem(newList, true);
   taskListRef.innerHTML = listTemplate + taskListRef.innerHTML;
 
   document.getElementById("title").value = "";
@@ -166,6 +153,7 @@ async function updateTask(task_id) {
       description,
       date,
     });
+
     if (isUpdated) {
       document.getElementById("title").value = "";
       document.getElementById("desc").value = "";
@@ -214,4 +202,18 @@ async function deleteTaskData(task_id) {
         console.log(er);
       });
   }
+}
+
+function searchTask(event) {
+  taskListRef.innerHTML = "";
+
+  var searchKeyWord = event.target.value;
+  var searchResult = taskListjson.filter((el) =>
+    el.title.toLowerCase().includes(searchKeyWord.toLowerCase())
+  );
+
+  searchResult.forEach((element) => {
+    var listTemplate = renderListItem(element, false);
+    taskListRef.innerHTML += listTemplate;
+  });
 }
